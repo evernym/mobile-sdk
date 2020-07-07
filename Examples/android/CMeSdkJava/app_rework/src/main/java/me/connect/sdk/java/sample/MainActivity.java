c502a230d42f1e6444c807831737f8192a969447
Alexey Shklyaev <alexey.shklyaev@dsr-corporation.com>
2020-07-07 11:44:35 +0300

    new sample app stub added

---
+++ Examples/android/CMeSdkJava/app_rework/src/main/java/me/connect/sdk/java/sample/MainActivity.java
@@ -0,0 +1,21 @@
+package me.connect.sdk.java.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.connect.sdk.java.sample.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ Examples/android/CMeSdkJava/app_rework/src/main/java/me/connect/sdk/java/sample/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package me.connect.sdk.java.sample.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
