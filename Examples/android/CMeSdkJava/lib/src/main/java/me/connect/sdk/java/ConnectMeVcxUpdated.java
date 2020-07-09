a0eea9da6e9c791c520c480e576723fe23fc4d96
Alexey Shklyaev <alexey.shklyaev@dsr-corporation.com>
2020-07-09 19:28:23 +0300

    MSDK-201 cloud wallet backups functionality for Android SDK

--- Examples/android/CMeSdkJava/lib/src/main/java/me/connect/sdk/java/ConnectMeVcxUpdated.java
+++ Examples/android/CMeSdkJava/lib/src/main/java/me/connect/sdk/java/ConnectMeVcxUpdated.java
@@ -1,5 +1,6 @@
 package me.connect.sdk.java;
 
+import android.content.Context;
 import android.util.Base64;
 import android.util.Log;
 
@@ -10,14 +11,23 @@ import com.evernym.sdk.vcx.connection.ConnectionApi;
 import com.evernym.sdk.vcx.credential.CredentialApi;
 import com.evernym.sdk.vcx.proof.DisclosedProofApi;
 import com.evernym.sdk.vcx.utils.UtilsApi;
+import com.evernym.sdk.vcx.wallet.WalletApi;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
+import java.util.zip.ZipOutputStream;
 
 import java9.util.concurrent.CompletableFuture;
 
@@ -470,6 +480,8 @@ public class ConnectMeVcxUpdated {
         int count = 1;
         try {
             Integer handle = CredentialApi.credentialDeserialize(serializedCredential).get();
+            // fixme all await methods should be cancellable
+            //       need to limit amount of retries or maximum time spent waiting
             while (true) {
                 Log.i(TAG, "Awaiting cred state change: attempt #" + count);
                 Integer state0 = CredentialApi.credentialUpdateState(handle).get();
@@ -825,6 +837,194 @@ public class ConnectMeVcxUpdated {
         return result;
     }
 
+    /**
+     * Create wallet backup
+     *
+     * @param sourceId  - String with institution's personal identification for the user
+     * @param backupKey - String representing the User's Key for securing  the exported wallet
+     * @return CompletableFuture containing JSON string with serialized wallet backup
+     */
+    public static @NonNull
+    CompletableFuture<String> createBackup(@NonNull String sourceId, @NonNull String backupKey) {
+        Log.i(TAG, "Creating backup");
+        CompletableFuture<String> result = new CompletableFuture<>();
+        try {
+            WalletApi.createWalletBackup(sourceId, backupKey)
+                    .exceptionally(t -> {
+                        Log.e(TAG, "Failed to create wallet backup: ", t);
+                        result.completeExceptionally(t);
+                        return null;
+                    })
+                    .thenAccept(backupHandle -> {
+                        if (backupHandle == null) {
+                            return;
+                        }
+                        try {
+                            WalletApi.serializeBackupWallet(backupHandle)
+                                    .exceptionally(t -> {
+                                        Log.e(TAG, "Failed to serialize wallet backup: ", t);
+                                        result.completeExceptionally(t);
+                                        return null;
+                                    })
+                                    .thenAccept(serializedHandle -> {
+                                        if (serializedHandle == null) {
+                                            return;
+                                        }
+                                        result.complete(serializedHandle);
+                                    });
+                        } catch (VcxException e) {
+                            result.completeExceptionally(e);
+                        }
+                    });
+        } catch (VcxException e) {
+            result.completeExceptionally(e);
+        }
+        return result;
+    }
+
+    /**
+     * Generate ZIP archive in directories relative to library root directory.
+     *
+     * @param context      Context
+     * @param relativePath relative path to file or directory to put into archive
+     * @param archivePath  relative archive path
+     * @return absolute path of archive
+     * @throws IOException in case there are problems during working with files
+     */
+    static String generateBackupArchive(Context context, String relativePath, String archivePath) throws IOException {
+        String walletPath = Utils.getRootDir(context) + "/" + relativePath;
+        String backupPath = Utils.getRootDir(context) + "/" + archivePath;
+        Utils.zipFiles(walletPath, backupPath);
+        return backupPath;
+    }
+
+    // Todo Context object should be taken from ConnectMeVcx after merge
+
+    /**
+     * Perform backup process
+     *
+     * @param context          context
+     * @param serializedBackup String containing serialized backup wallet handle
+     * @return CompletableFuture containig serialized backup wallet handle
+     */
+    public static @NonNull
+    CompletableFuture<String> performBackup(@NonNull Context context, @NonNull String serializedBackup) {
+        Log.i(TAG, "Performing backup");
+        CompletableFuture<String> result = new CompletableFuture<>();
+        try {
+            String pathToArchive = generateBackupArchive(context, "indy_client", "backup-" + System.currentTimeMillis());
+            WalletApi.deserializeBackupWallet(serializedBackup)
+                    .exceptionally(t -> {
+                        Log.e(TAG, "Failed to deserialize wallet backup: ", t);
+                        result.completeExceptionally(t);
+                        return null;
+                    })
+                    .thenAccept(backupHandle -> {
+                        if (backupHandle == null) {
+                            return;
+                        }
+                        try {
+                            WalletApi.backupWalletBackup(backupHandle, pathToArchive)
+                                    .exceptionally(t -> {
+                                        Log.e(TAG, "Failed to backup wallet: ", t);
+                                        result.completeExceptionally(t);
+                                        return null;
+                                    })
+                                    .thenAccept(nothing -> {
+                                        try {
+                                            WalletApi.serializeBackupWallet(backupHandle)
+                                                    .exceptionally(t -> {
+                                                        Log.e(TAG, "Failed to serialize wallet backup: ", t);
+                                                        result.completeExceptionally(t);
+                                                        return null;
+                                                    })
+                                                    .thenAccept(serializedHandle -> {
+                                                        if (serializedHandle == null) {
+                                                            return;
+                                                        }
+                                                        result.complete(serializedHandle);
+                                                    });
+                                        } catch (VcxException e) {
+                                            result.completeExceptionally(e);
+                                        }
+                                    });
+                        } catch (VcxException e) {
+                            result.completeExceptionally(e);
+                        }
+                    });
+        } catch (Exception e) {
+            result.completeExceptionally(e);
+        }
+        return result;
+    }
+
+
+    /**
+     * Restores wallet
+     *
+     * @param context   context
+     * @param backupKey backup wallet key used during backup wallet creation
+     * @return CompletableFuture with nothing
+     */
+    public static @NonNull
+    CompletableFuture<Void> restoreBackup(@NonNull Context context, @NonNull String backupKey) {
+        // Todo Context object should be taken from ConnectMeVcx after merge
+        Log.i(TAG, "Restoring backup");
+        CompletableFuture<Void> result = new CompletableFuture<>();
+
+        try {
+            JSONObject config = new JSONObject();
+            config.put("wallet_name", "restoredWalletName");
+            config.put("wallet_key", "restoredWalletKey");
+            config.put("backup_key", backupKey);
+            config.put("exported_wallet_path", Utils.getRootDir(context) + "/restored");
+            WalletApi.restoreWalletBackup(config.toString())
+                    .exceptionally(t -> {
+                        Log.e(TAG, "Failed to backup wallet: ", t);
+                        result.completeExceptionally(t);
+                        return null;
+                    })
+                    .thenAccept(nothing -> {
+                        if (nothing == null) {
+                            return;
+                        }
+                        result.complete(null);
+                    });
+        } catch (Exception e) {
+            result.completeExceptionally(e);
+        }
+
+        return result;
+    }
+
+    /**
+     * Loops indefinitely until wallet backup status is not changed
+     *
+     * @param serializedBackup string containing serialized wallet backup
+     * @return string containing serialized wallet backup
+     */
+    public static @NonNull
+    String awaitBackupStatusChange(@NonNull String serializedBackup) {
+        Log.i(TAG, "Awaiting backup state change");
+        int count = 1;
+        try {
+            Integer handle = WalletApi.deserializeBackupWallet(serializedBackup).get();
+            while (true) {
+                Log.i(TAG, "Awaiting backup state change: attempt #" + count);
+                Integer state = WalletApi.updateWalletBackupState(handle).get();
+                Log.i(TAG, "Awaiting backup state change: update state=" + state);
+                if (state == 4 || state == 2) {
+                    return WalletApi.serializeBackupWallet(handle).get();
+                }
+                count++;
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to await proof state", e);
+            e.printStackTrace();
+        }
+        return serializedBackup;
+    }
 
     /**
      * Temporary method to parse structured question message JSON string and extract {@link StructuredMessage} from it.
