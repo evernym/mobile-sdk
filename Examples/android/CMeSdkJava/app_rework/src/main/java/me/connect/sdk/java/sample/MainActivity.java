761b881aeef70f34084246bdbcaabf8df7220609
Alexey Shklyaev <alexey.shklyaev@dsr-corporation.com>
2020-07-20 14:46:18 +0300

    added init status handling

--- Examples/android/CMeSdkJava/app_rework/src/main/java/me/connect/sdk/java/sample/MainActivity.java
+++ Examples/android/CMeSdkJava/app_rework/src/main/java/me/connect/sdk/java/sample/MainActivity.java
@@ -3,11 +3,17 @@ package me.connect.sdk.java.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import me.connect.sdk.java.AgencyConfig;
+import me.connect.sdk.java.ConnectMeVcx;
 import me.connect.sdk.java.sample.databinding.MainActivityBinding;
 import me.connect.sdk.java.sample.ui.main.MainPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+    private ConnectMeVcx sdk;
 
 
     private MainActivityBinding viewBinding;
@@ -21,5 +27,29 @@ public class MainActivity extends AppCompatActivity {
         MainPagerAdapter adapter = new MainPagerAdapter(getSupportFragmentManager());
         viewBinding.pager.setAdapter(adapter);
         viewBinding.tabLayout.setupWithViewPager(viewBinding.pager);
+
+        initSdk();
+    }
+
+    private void initSdk() {
+        Toast.makeText(this, "Started SDK initialization", Toast.LENGTH_SHORT).show();
+        sdk = ConnectMeVcx.builder()
+                .withAgency(AgencyConfig.DEFAULT)
+                .withGenesisPool(Constants.POOL_TXN_GENESIS)
+                .withWalletName(Constants.WALLET_NAME)
+                .withContext(this)
+                .build();
+        // Todo progress bar could be added
+        sdk.init().handleAsync((res, err) -> {
+            String message;
+            if (err == null) {
+                message = "SDK initialized successfully.";
+            } else {
+                message = "SDK was not initialized!";
+                Log.e(TAG, "Sdk not initialized: ", err);
+            }
+            runOnUiThread(() -> Toast.makeText(this, message, Toast.LENGTH_SHORT).show());
+            return null;
+        });
     }
 }
