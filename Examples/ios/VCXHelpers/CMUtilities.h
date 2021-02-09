b36f5564576c07fbbff75399a1d729c5435bebba
Pedja Jevtic <designbyheart@gmail.com>
2021-02-09 16:38:49 +0100

    Added missing helper classes

---
+++ Examples/ios/VCXHelpers/CMUtilities.h
@@ -0,0 +1,35 @@
+//
+//  Utilities.h
+//  CMeSdkObjc
+//
+//  Created by Predrag Jevtic on 5/28/20.
+//  Copyright © 2020 Evernym Inc. All rights reserved.
+//
+
+#ifndef Utilities_h
+#define Utilities_h
+
+typedef void (^ResponseBlock)(NSString *successMessage, NSError *error);
+typedef void (^ResponseWithObject)(NSDictionary *responseObject, NSError *error);
+typedef void (^ResponseWithArray)(NSArray *responseArray, NSError *error);
+
+@interface CMUtilities : NSObject
+
++(NSString*)toJsonString:(NSDictionary*)json;
++(NSString*)arrayToJsonString: (NSArray*) json;
++(NSString*)dictToJsonString: (NSDictionary*) json;
++(NSString*)encodeStringTo64: (NSString*)fromString;
++(NSData*)decode64String: (NSString*)fromString;
+
++(NSDictionary*)jsonToDictionary: (NSString*)json;
++(NSArray*)jsonToArray: (NSString*)json;
+
++(void)printError: (NSError*)error;
++(void)printErrorMessage: (NSString*)error;
++(void)printSuccess: (NSArray*)message;
++(void)sendPostRequest: (NSString*)serverURL withBody: (NSDictionary*) data andCompletion: (ResponseWithObject) completionBlock;
+
+@end
+
+#endif /* Utilities_h */
+
---
+++ Examples/ios/VCXHelpers/StagingPoolTxnGenesis.h
@@ -0,0 +1,15 @@
+//
+//  StagingPoolTxnGenesis.h
+//  CMeSdkObjc
+//
+//  Created by Predrag Jevtic on 16/09/2020.
+//  Copyright © 2020 Evernym Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#ifndef StagingPoolTxnGenesis_h
+#define StagingPoolTxnGenesis_h
+
+extern NSString * const stagingPoolTxnGenesisDef;
+
+#endif
