55f294a994e151f34a599d67321b5784dbe23a50
Andrey Shevchenko <andrey.shevchenko@evernym.com>
2021-07-06 13:23:28 +0300

    Added logger with write to file

---
+++ examples/ios/CMeSdkObjc/CMeSdkObjc/Helpers/Log.h
@@ -0,0 +1,13 @@
+//
+//  Log.h
+//  CMeSdkObjc
+//
+//  Created by Evernym on 06.07.2021.
+//  Copyright © 2021 Norman Jarvis. All rights reserved.
+//
+//
+
+#define NSLog(args...) _Log(@"DEBUG ", __FILE__,__LINE__,__PRETTY_FUNCTION__,args);
+@interface Log : NSObject
+void _Log(NSString *prefix, const char *file, int lineNumber, const char *funcName, NSString *format,...);
+@end
