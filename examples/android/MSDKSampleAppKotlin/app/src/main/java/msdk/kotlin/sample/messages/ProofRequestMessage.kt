c2dfc2d92fb7a9b309aac13f2460e925b855c68c
Andrey Shevchenko <andrey.shevchenko@evernym.com>
2021-11-17 15:58:12 +0300

    Added extract method for android side

--- examples/android/MSDKSampleAppKotlin/app/src/main/java/msdk/kotlin/sample/messages/ProofRequestMessage.kt
+++ examples/android/MSDKSampleAppKotlin/app/src/main/java/msdk/kotlin/sample/messages/ProofRequestMessage.kt
@@ -1,6 +1,7 @@
 package msdk.kotlin.sample.messages
 
 import android.util.Base64
+import com.evernym.sdk.vcx.utils.UtilsApi
 import msdk.kotlin.sample.utils.CommonUtils
 import org.json.JSONArray
 import org.json.JSONException
@@ -35,23 +36,8 @@ class ProofRequestMessage(
 
         fun decodeProofRequestAttach(proofAttach: JSONObject): JSONObject? {
             try {
-                val requestAttachCode =
-                    proofAttach.getString("request_presentations~attach")
-                val requestsAttachItems = JSONArray(requestAttachCode)
-                if (requestsAttachItems.length() == 0) {
-                    return null
-                }
-                val requestsAttachItem = requestsAttachItems.getJSONObject(0)
-                val requestsAttachItemData = requestsAttachItem.getJSONObject("data")
-                val requestsAttachItemBase =
-                    requestsAttachItemData.getString("base64")
-                val requestAttachDecode = String(
-                    Base64.decode(
-                        requestsAttachItemBase,
-                        Base64.NO_WRAP
-                    )
-                )
-                return CommonUtils.convertToJSONObject(requestAttachDecode)
+                val attachment = UtilsApi.vcxExtractAttachedMessage(proofAttach.toString()).get()
+                return CommonUtils.convertToJSONObject(attachment)
             } catch (e: JSONException) {
                 e.printStackTrace()
             }
--- examples/android/MSDKSampleAppKotlin/app/src/main/java/msdk/kotlin/sample/types/MessageAttachment.kt
+++ examples/android/MSDKSampleAppKotlin/app/src/main/java/msdk/kotlin/sample/types/MessageAttachment.kt
@@ -1,10 +1,12 @@
 package msdk.kotlin.sample.types
 
-import android.util.Base64
+import com.evernym.sdk.vcx.VcxException
+import com.evernym.sdk.vcx.utils.UtilsApi
 import msdk.kotlin.sample.utils.CommonUtils
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
+import java.util.concurrent.ExecutionException
 
 class MessageAttachment(var type: String, var data: JSONObject) {
     val isCredentialAttachment: Boolean
@@ -16,34 +18,61 @@ class MessageAttachment(var type: String, var data: JSONObject) {
     companion object {
         fun parse(invite: String?): MessageAttachment? {
             try {
-                val json = CommonUtils.convertToJSONObject(invite)
-                if (json == null || !json.has("request~attach")) {
+                val attachment = UtilsApi.vcxExtractAttachedMessage(fixForRequestField(invite)).get()
+                val attachmentJson = CommonUtils.convertToJSONObject(attachment)!!
+                attachmentJson.put("@id", getIdFromInvite(invite))
+                val type = attachmentJson.getString("@type")
+                return MessageAttachment(type, attachmentJson)
+            } catch (e: VcxException) {
+                e.printStackTrace()
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            } catch (e: ExecutionException) {
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        private fun fixForRequestField(invite: String?): String? {
+            try {
+                val fixInvite = JSONObject()
+                val inviteJson = CommonUtils.convertToJSONObject(invite)
+                fixInvite.put("goal", inviteJson?.getString("goal"))
+                fixInvite.put("service", inviteJson?.getJSONArray("service"))
+                fixInvite.put("@id", inviteJson?.getString("@id"))
+                fixInvite.put("@type", inviteJson?.getString("@type"))
+                fixInvite.put("profileUrl", inviteJson?.getString("profileUrl"))
+                fixInvite.put("handshake_protocols", inviteJson?.getJSONArray("handshake_protocols"))
+                fixInvite.put("label", inviteJson?.getString("label"))
+                fixInvite.put("goal_code", inviteJson?.getString("goal_code"))
+                fixInvite.put("public_did", inviteJson?.getString("public_did"))
+                fixInvite.put("requests~attach", inviteJson?.getJSONArray("request~attach"))
+                return fixInvite.toString()
+            } catch (exp: JSONException) {
+                exp.printStackTrace()
+            }
+            return null
+        }
+
+        private fun getIdFromInvite(invite: String?): String? {
+            try {
+                val inviteJson = CommonUtils.convertToJSONObject(invite)
+                if (inviteJson != null && !inviteJson.has("request~attach")) {
                     return null
                 }
-                val requestAttachCode = json.getString("request~attach")
+                val requestAttachCode = inviteJson!!.getString("request~attach")
                 val requestsAttachItems = JSONArray(requestAttachCode)
                 if (requestsAttachItems.length() == 0) {
                     return null
                 }
                 val requestsAttachItem = requestsAttachItems.getJSONObject(0)
-                val requestsAttachItemData = requestsAttachItem.getJSONObject("data")
-                val requestsAttachItemBase =
-                    requestsAttachItemData.getString("base64")
-                val requestAttachDecode = String(
-                    Base64.decode(
-                        requestsAttachItemBase,
-                        Base64.NO_WRAP
-                    )
-                )
-                val attachment = CommonUtils.convertToJSONObject(requestAttachDecode) ?: return null
-                attachment.put("@id", requestsAttachItem.getString("@id"))
-                val type = attachment.getString("@type")
-                return MessageAttachment(type, attachment)
-            } catch (e: JSONException) {
-                e.printStackTrace()
+                return requestsAttachItem.getString("@id")
+            } catch (exp: JSONException) {
+                exp.printStackTrace()
             }
             return null
         }
     }
-
 }
